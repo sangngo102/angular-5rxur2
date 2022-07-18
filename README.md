ea072f44d4a345830f56d947122ac58cf001859f
sangngo102 <vansangnghiaky@gmail.com>
2022-07-18 13:02:23 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-5rxur2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-5rxur2)
---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name = 'Angular';
+
+  object = {
+    name: 'Sang Ngo',
+    Age: 22,
+  };
+  hello(){
+  }
+  handle(){
+    console.log('hello');
+  }
+}
